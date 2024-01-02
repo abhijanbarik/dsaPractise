0b6dec17703d3f139e450300ed8e24a917ee54cc
abhijanbarik <cdhyper22@gmail.com>
2024-01-02 22:28:14 +0530

    day3.1

---
+++ easyProblems/src/PalindromeNumber.java
@@ -0,0 +1,27 @@
+public class PalindromeNumber {
+
+    public static void main(String[] args){
+
+        int num = 0;
+
+        boolean resBack = isPalindrome(num);
+
+        System.out.println(resBack);
+    }
+
+    public static boolean isPalindrome(int x) {
+
+        int real = x, num = 0;
+
+        while(x != 0){
+
+            int rem = x % 10;
+
+            x /= 10;
+
+            num = (num * 10) + rem;
+        }
+
+        return real == num && real >= 0;
+    }
+}
