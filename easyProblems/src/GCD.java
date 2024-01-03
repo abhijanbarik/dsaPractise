aeb61441af942621f82ac55d91cc83164258a86b
abhijanbarik <cdhyper22@gmail.com>
2024-01-03 20:27:41 +0530

    day4

--- easyProblems/src/GCD.java
+++ easyProblems/src/GCD.java
@@ -1,7 +1,7 @@
 public class GCD {
     public static void main(String[] args){
 
-        int num1 = 15, num2 = 10;
+        int num1 = 10, num2 = 52;
 
         int getRes = gcd(num1, num2);
 
@@ -9,8 +9,24 @@ public class GCD {
     }
 
     public static int gcd(int a, int b){
-
+        //Recursive approach
         if(b == 0) return a;
         return gcd(b, a%b);
     }
+
+//    public static int gcd(int a, int b){
+//
+//        // Euclidean algorithm for GCD
+//
+//        while(a > 0 && b > 0){
+//
+//            if(a > b) a = a % b;
+//
+//            else b = b % a;
+//        }
+//
+//        if (a == 0) return b;
+//
+//        return a;
+//    }
 }
