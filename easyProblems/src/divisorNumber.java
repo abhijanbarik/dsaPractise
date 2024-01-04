cb802ee3d165d0a7ee92dbb03a47b1173707f6d7
abhijanbarik <cdhyper22@gmail.com>
2024-01-04 23:59:22 +0530

    day5

---
+++ easyProblems/src/divisorNumber.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class divisorNumber {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+
+        // Brute force
+//        for(int i=1; i <= num; i++) {
+//
+//            if (num % i == 0) System.out.println(i);
+//        }
+
+
+        List<Integer> arr = new ArrayList<>();
+
+                       // i * i <= n (other way of writing it)
+        for(int i = 1; i <= Math.sqrt(num); i++){
+
+            if(num % i == 0) {
+
+                arr.add(i);
+
+                if ((num / i) != i) {
+
+                    arr.add(num/i);
+                }
+            }
+        }
+        Collections.sort(arr);
+        System.out.println(arr);
+    }
+}
