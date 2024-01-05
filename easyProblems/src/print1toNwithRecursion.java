68d0b0be98efa7e1207606cbaf742550fe795bd7
abhijanbarik <cdhyper22@gmail.com>
2024-01-05 21:16:38 +0530

    day6

---
+++ easyProblems/src/print1toNwithRecursion.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class print1toNwithRecursion {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        int[] res;
+        res = recFunc(num, arr);
+        System.out.println(Arrays.toString(res));
+    }
+
+    static int[] recFunc(int n, int[] arr){
+
+        if(n <= 0) return arr;
+
+        recFunc(n - 1, arr);
+
+        arr[n-1] = n;
+
+        return arr;
+    }
+}
