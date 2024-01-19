c1ce4879ed359128ff8d0f9854c980d937adbf3e
abhijanbarik <cdhyper22@gmail.com>
2024-01-19 10:24:49 +0530

    day11.1

---
+++ easyProblems/src/test.java
@@ -0,0 +1,25 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        int N = 36, l = 1, cnt = 0;
+
+        int r = N;
+
+        while (l < r){
+
+            int mid = (int) Math.floor((l + r)/2);
+
+            cnt = cnt + 1;
+
+            if((mid * mid) == N) {
+                System.out.println(cnt);
+                break;
+            }
+
+            if((mid * mid) < N) l = mid + 1;
+
+            if((mid * mid) > N) r = mid - 1;
+        }
+    }
+}
