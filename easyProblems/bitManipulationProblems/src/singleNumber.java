3b30e956bf4d96f7a4455c3f21505e4ecb591da2
abhijanbarik <cdhyper22@gmail.com>
2024-01-21 17:33:49 +0530

    day12

---
+++ easyProblems/bitManipulationProblems/src/singleNumber.java
@@ -0,0 +1,21 @@
+public class singleNumber {
+
+    public static void main(String[] args) {
+
+        int[] A = new int[]{2, 3, 5, 2, 5, 2, 12, 11, 16, 11, 12, 16};
+
+        System.out.println(singleDigit(A));
+    }
+
+    static int singleDigit(int[] arr){
+
+        int res = 0;
+
+        for(int i : arr){
+
+            res = res ^ i;
+        }
+
+        return res;
+    }
+}
