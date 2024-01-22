9b07e200f7ae15048ad695cbbe2024aae6528b4f
abhijanbarik <cdhyper22@gmail.com>
2024-01-22 08:43:39 +0530

    day13

---
+++ easyProblems/bitManipulationProblems/src/NumberOf1Bits.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class NumberOf1Bits {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any Random Number: ");
+
+        int num = sc.nextInt();
+
+        System.out.println(countBits(num));
+    }
+
+    static int countBits(int num) {
+
+         // Optimised Way TC -> O(Log(N)) & SC -> O(1)
+
+        int count = 0;
+
+        while(num > 0){
+            num = num & (num - 1);
+            count++;
+        }
+
+        return count;
+
+
+//        TC: O(N) & SC: O(1) -> Brute Force
+//        int count = 0;
+//        while((num > 0) && (num > 1)){
+//            int rem = num % 2;
+//            num = num / 2;
+//            arr.add(rem);
+//        }
+//        for(int i : arr) if(arr.contains(1)) count++;
+//        if(count == 0) count = 1;
+//        return count;
+    }
+}
