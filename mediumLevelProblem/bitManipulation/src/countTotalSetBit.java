e9a71dc181f7dc8648602fc086774d95825a2492
Abhijan Barik <38183383+abhijanbarik@users.noreply.github.com>
2024-01-25 19:38:31 +0530

    Update countTotalSetBit.java

--- mediumLevelProblem/bitManipulation/src/countTotalSetBit.java
+++ mediumLevelProblem/bitManipulation/src/countTotalSetBit.java
@@ -10,7 +10,7 @@ public class countTotalSetBit {
     static int countSetBit(int num) {
 
         // TC: O(1) & SC: O(1)
-        long N = num + 1, mod = 1000*1000*1000 + 7;
+        long N = num + 1, mod = 1000000007;
 
         long ans = 0, pow2 = 1;
 
