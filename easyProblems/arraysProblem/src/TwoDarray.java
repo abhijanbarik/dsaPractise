2f078b78821f31fdfb48d5208b9e249a54e278ad
abhijanbarik <cdhyper22@gmail.com>
2024-02-08 12:47:27 +0530

    day24.1

---
+++ easyProblems/arraysProblem/src/TwoDarray.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class TwoDarray {
+
+    public static void main(String[] args) {
+
+
+        int[][] arr = new int[][]{{1,2,3,4}, {5,6,7,8}, {9,2,3,4}};
+
+        System.out.println(Arrays.toString(solve(arr)));
+    }
+
+    static int[] solve(int[][] A){
+
+        int[] res = new int[A[0].length];
+
+        for(int col = 0; col < A[0].length; col++){
+
+            int sum = 0;
+
+            for(int row = 0; row < A.length; row++){
+
+                sum = sum + A[row][col];
+            }
+            res[col] = sum;
+        }
+        return res;
+    }
+}
