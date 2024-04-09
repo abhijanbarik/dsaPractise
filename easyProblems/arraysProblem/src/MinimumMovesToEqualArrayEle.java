fb7458f760a5e7bc9333afc328d79464fe5c81f6
abhijanbarik <cdhyper22@gmail.com>
2024-04-09 12:04:43 +0530

    day30.1

---
+++ easyProblems/arraysProblem/src/MinimumMovesToEqualArrayEle.java
@@ -0,0 +1,29 @@
+
+public class MinimumMovesToEqualArrayEle {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 2, 3};
+
+        int ans = minMoves(arr);
+
+        System.out.println(ans);
+    }
+
+    static int minMoves(int[] nums) {
+
+        int minValue = Integer.MAX_VALUE, count = 0;
+
+        for (int num : nums) {
+
+            minValue = Math.min(minValue, num);
+        }
+
+        for (int num : nums) {
+
+            count = count + (num - minValue);
+        }
+
+        return count;
+    }
+}
