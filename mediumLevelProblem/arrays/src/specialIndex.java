2e8ededf9621cdf2e04c0898f811c01660f50370
Abhijan Barik <38183383+abhijanbarik@users.noreply.github.com>
2024-05-05 22:33:56 +0530

    Update specialIndex.java

--- mediumLevelProblem/arrays/src/specialIndex.java
+++ mediumLevelProblem/arrays/src/specialIndex.java
@@ -67,9 +67,9 @@ public class specialIndex {
         
         // int oddSum = 0;
         
-        // for(int i=0; i<A.length; i++) {
+        // for(int i = 0; i < A.length; i++) {
             
-        //     if(i%2 == 0)
+        //     if((i & 1) == 0)
         //         evenSum += A[i];
             
         //     else
@@ -78,9 +78,9 @@ public class specialIndex {
         
         // int count = 0;
         
-        // for(int i=0;i<A.length; i++) {
+        // for(int i = 0; i < A.length; i++) {
            
-        //     if(i%2 == 0) {
+        //     if((i & 1) == 0) {
                 
         //         evenSum -= A[i];
                 
