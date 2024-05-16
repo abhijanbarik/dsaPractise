2627c25d05711a878f0d72465f2c473f23637467
abhijanbarik <cdhyper22@gmail.com>
2024-05-16 10:43:21 +0530

    day38.2

---
+++ StringProblems/EasyProblems/src/ReverseString.java
@@ -0,0 +1,37 @@
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String S = "Google";
+
+        System.out.println(solve(S));
+    }
+
+    public static String solve(String A){
+
+//        TC: O(N) & SC: O(N)
+
+        StringBuilder sb = new StringBuilder(A);
+
+        int start = 0, end = A.length()-1;
+
+        while(start <= end){
+
+            char temp = sb.charAt(start);
+
+            sb.setCharAt(start, A.charAt(end));
+
+            sb.setCharAt(end, temp);
+
+            start++;
+
+            end--;
+        }
+
+        return sb.toString();
+
+//        StringBuilder sb = new StringBuilder(A).reverse();
+//        return sb.toString();
+    }
+}
