37b3ef5fa2b9f876e6aa747ee979372695c30fcd
abhijanbarik <cdhyper22@gmail.com>
2024-05-18 23:57:20 +0530

    day39.1

---
+++ StringProblems/EasyProblems/src/ChangeCharacter.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class ChangeCharacter {
+
+    public static void main(String[] args) {
+
+        String str = "abcabbccd";
+
+        int B = 3;
+
+        int ans = solve(str, B);
+
+        System.out.println(ans);
+    }
+
+    public static int solve(String A, int B){
+
+        // TC: O(N) & SC: O(1);
+
+        int N = A.length();
+
+        int[] arr = new int[26];
+
+        for(int i = 0; i < N; i++){
+
+            int x = A.charAt(i) - 97;
+
+            arr[x]++;
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+
+        for(int i = 0; i < 26; i++){
+
+            sum = sum + arr[i];
+
+            if(sum > B) return 26 - i;
+        }
+
+        return -1;
+    }
+}
