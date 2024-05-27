7d8b97864ff69030821c654741fb69d707d3d90e
abhijanbarik <cdhyper22@gmail.com>
2024-05-27 12:31:09 +0530

    day2

---
+++ patternsProblem/src/Pattern8.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Pattern8 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+
+        int N = sc.nextInt();
+
+        patternEight(N);
+
+    }
+
+    public static void patternEight(int N){
+
+        for(int row = 0; row < N; row++){
+
+            for(int sp1 = 0; sp1 < row; sp1++){
+
+                System.out.print(" ");
+            }
+
+            for(int col = 0; col < (2 * N) - (2 * row + 1); col++){
+
+                System.out.print("*");
+            }
+
+            for(int sp2 = 0; sp2 < row; sp2++){
+
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
