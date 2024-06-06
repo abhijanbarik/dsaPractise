da93bbe762ad76754930c4f31f1857ccd7664666
abhijanbarik <cdhyper22@gmail.com>
2024-06-06 09:38:30 +0530

    day 37

--- mediumLevelProblem/bitManipulation/src/reverseBits.java
+++ mediumLevelProblem/bitManipulation/src/reverseBits.java
@@ -10,7 +10,6 @@ public class reverseBits {
 
     public static long returnBit(long A) {
 
-
         // Advance level is not as good as brute force TC: O(1) & SC: O(1)
         
         long rev = 0;
