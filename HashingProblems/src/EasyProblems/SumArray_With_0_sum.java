0c4b25156908752974c9f2de46c73ca2659e8631
abhijanbarik <cdhyper22@gmail.com>
2024-06-19 11:35:35 +0530

    day 42

---
+++ HashingProblems/src/EasyProblems/SumArray_With_0_sum.java
@@ -0,0 +1,40 @@
+package EasyProblems;
+import java.util.HashSet;
+
+public class SumArray_With_0_sum {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, 2, -2, 4, -4};
+
+        int ans = solve(arr);
+
+        System.out.println(ans);
+    }
+
+    public static int solve(int[] A){
+
+//        TC: O(N) & SC: O(N)
+        /* We can also solve this by PreFix-Sum method */
+
+        HashSet<Integer> hs = new HashSet<>();
+
+        int sum = 0;
+
+        for(int i : A){
+
+            sum = sum + i;
+
+            if(hs.contains(sum)){
+
+                return 1;
+
+            }else{
+
+                hs.add(i);
+            }
+        }
+
+        return 0;
+    }
+}
