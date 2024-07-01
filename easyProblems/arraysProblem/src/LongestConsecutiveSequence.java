6cb11bc8d3ecfa0a884d8fe6335ee56128c05c59
abhijanbarik <cdhyper22@gmail.com>
2024-07-01 11:43:16 +0530

    day 50

---
+++ easyProblems/arraysProblem/src/LongestConsecutiveSequence.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{100, 4, 200, 1, 3, 2};
+
+        int ans = solve(arr);
+
+        System.out.println(ans);
+    }
+
+    public static int solve(int[] A){
+
+        int N = A.length, maxVal = Integer.MIN_VALUE;
+
+        HashSet<Integer> hs = new HashSet<>();
+
+        for(int i = 0; i < N; i++){
+
+            hs.add(A[i]);
+        }
+
+        for(int i = 0; i < N; i++){
+
+            if(!hs.contains(A[i] - 1)){
+
+                int val = A[i];
+
+                int count = 1;
+
+                while(hs.contains(val + 1)){
+
+                    count++;
+
+                    val++;
+                }
+
+                maxVal = Math.max(count, maxVal);
+            }
+        }
+
+        return maxVal;
+    }
+}
